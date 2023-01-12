070354acdefd47e00f81793f8a32a10d6f3ac22b
maksi <maksi@DESKTOP-GR9M90C>
2023-01-12 10:37:13 +0300

    Добавьте файлы проекта.

---
+++ 4 ergergfeg/4 ergergfeg.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main() {
+    system("chcp 1251"); 
+    ifstream fin; 
+    fin.open("MyInput.txt"); 
+    string str, str2; 
+    int count = 0; 
+    while (!fin.eof()) { 
+        str = ""; 
+        getline(fin, str); 
+        istringstream iss(str); 
+        string str2; 
+        while (iss >> str2) { 
+            count++; 
+        }
+    }
+    ofstream fout; 
+    fout.open("MyAnswer.txt"); 
+    fout << count - 1; 
+    fout.close();
+    fin.close();
+    return 0;
+}
